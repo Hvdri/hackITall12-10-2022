34c3c29a35f8f0a584d6f1ea9c552350cc802d9f
PatrascuIoana <al3xandra.ioana0327@gmail.com>
2022-12-10 23:20:58 +0200

    navbar

--- frontend/src/sharepages/navbar/Navbar.jsx
+++ frontend/src/sharepages/navbar/Navbar.jsx
@@ -7,19 +7,19 @@ import './Navbar.css'
 function Navbar() {
   return (
 
-    <nav className="navbar fixed-top bg-light">
-        <div className="">
+    <nav className="navbar fixed-top bg-light d-flex justify-content-between ">
+        <div className="nav-item ">
             <a>
                 <IoIosArrowBack />
             </a>
         </div>
-        <div className=''>
+        <div className='nav-item'>
             <a class="navbar-brand" href="#">
                 <img src="https://cdn0.erstegroup.com/content/dam/ro/bcr/common/logo-svg/BCR_web_internal-material.svg" alt="BCR Banca Comerciala Romana" width="102" height="38"/>
             </a>
         </div>
-        <div className="">
-            <a>hello</a>
+        <div className="nav-item">
+            <a>EN|RO</a>
         </div>
     </nav>
 
