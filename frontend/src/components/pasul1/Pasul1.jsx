6f26bcd5a508c26045b560fbe6f08add2548bb13
PatrascuIoana <al3xandra.ioana0327@gmail.com>
2022-12-10 14:37:38 +0200

    Added Navbar + Footer

--- frontend/src/components/pasul1/Pasul1.jsx
+++ frontend/src/components/pasul1/Pasul1.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
+import '../../sharepages/footer/Footer';
+import '../../sharepages/navbar/Navbar';
+import Footer from '../../sharepages/footer/Footer';
+import Navbar from '../../sharepages/navbar/Navbar';
+
 function Pasul1 (){
     return (
-    <div>
+      <div>
+      <Navbar />
         <h1>Welcome to Pasul 1!</h1>
 
         
         <li>
           <Link to="/Pasul2">Pasul2</Link>
         </li>
+        <Footer />
     </div>
     );
 
