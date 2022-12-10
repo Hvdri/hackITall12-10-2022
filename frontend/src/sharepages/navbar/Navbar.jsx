37a60f6550f0f6fcd51b3ac7f32b0c30859d5169
PatrascuIoana <al3xandra.ioana0327@gmail.com>
2022-12-10 23:01:04 +0200

    ca sa nu crape

--- frontend/src/sharepages/navbar/Navbar.jsx
+++ frontend/src/sharepages/navbar/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { IoIosArrowBack } from 'react-icons/io';
+import './Navbar.css'
+
+
 
 function Navbar() {
   return (
-    
+
     <nav className="navbar fixed-top bg-light">
         <div className="">
-            <a style={{ textDecoration: 'none' }}>hello</a>
+            <a>
+                <IoIosArrowBack />
+            </a>
         </div>
         <div className=''>
-            <a style={{ textDecoration: 'none' }}>hello</a>
+            <a class="navbar-brand" href="#">
+                <img src="https://cdn0.erstegroup.com/content/dam/ro/bcr/common/logo-svg/BCR_web_internal-material.svg" alt="BCR Banca Comerciala Romana" width="102" height="38"/>
+            </a>
         </div>
         <div className="">
-            <a style={{ textDecoration: 'none' }}>hello</a>
+            <a>hello</a>
         </div>
     </nav>
 
