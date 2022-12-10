08b6ea52c80596932cb44f4ff908a1fd73a6c1f6
PatrascuIoana <al3xandra.ioana0327@gmail.com>
2022-12-10 16:35:31 +0200

    Pasul 1

--- frontend/src/components/pasul1/Pasul1.jsx
+++ frontend/src/components/pasul1/Pasul1.jsx
@@ -10,11 +10,46 @@ function Pasul1 (){
     return (
       <div>
       <Navbar />
+      <div className='container-title'>
         <h1>Welcome to Pasul 1!</h1>
+        <p>Alege un motiv pentru care programezi vizita in unitate</p>
+      </div>
 
+      <div className='container'>
+        <div className='btn'>
+          <button className='btn'>
+              Depunere sau retragere numerar
+          </button>
+        </div>
+
+        <div className='btn'>
+          <button>
+              Operatiune fara numerar
+          </button>
+        </div>
+
+        <div className='btn'>
+          <button>
+              Credit
+          </button>
+        </div>
+
+        <div className='btn'>
+          <button>
+            Economisire
+          </button>
+        </div>
+
+        <div className='btn'>
+          <button>
+            Asigurare
+          </button>
+        </div>
+
+      </div>
         
         <li>
-          <Link to="/Pasul2">Pasul2</Link>
+          <Link to="/Pasul2">Continua</Link>
         </li>
         <Footer />
     </div>
