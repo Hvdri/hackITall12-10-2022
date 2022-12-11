408b9af290883d07c1e4106c5c01772a3ccf485e
Horia <horia.marinescu12@gmail.com>
2022-12-11 09:47:42 +0200

    m

--- frontend/src/components/pasul1/Pasul1.jsx
+++ frontend/src/components/pasul1/Pasul1.jsx
@@ -11,6 +11,7 @@ import '../../App'
 import { useState } from 'react';
 // import axios from "axios";
 
+
 function Pasul1 (){
   axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
@@ -24,16 +25,7 @@ function Pasul1 (){
   
     const handleSumbit = (e) => {
         e.preventDefault();
-        // axios({
-        //   method: 'POST',
-        //   url: `http://localhost/hackITall12-10-2022/backend/main.php?apicall=Motiv`,
-        //   headers: { 'Content-Type': 'application/json', },
-        //   data: result
-        // })
-        //   .then(result => {
-        //     console.log(result);
-            
-        //   })
+        
         axios.post('http://localhost/hackITall12-10-2022/backend/main.php?apicall=Motiv', {
           dataMotiv: dataMotiv
         })
@@ -47,6 +39,21 @@ function Pasul1 (){
     };
   
     let op1 = "Operatiune cu numerar";
+    let op2 = "Plata rata credit ";
+    let op3 = "Operatiune fara numerar";
+    let op4 = "Deschidere cont curent ";
+    let op5 = "Deschidere cont minori ";
+    let op6 = "Deschidere conturi refugiati ";
+    let op7 = "Suport utilizare aplicatie George ";
+    let op8 = "Diagnostic financiar gratuit ";
+    let op9 = "Credit de nevoi personale ";
+    let op10 = "Credit ipotecar ";
+    let op11 = "Economisire  ";
+    let op12 = "Contracte BpL  ";
+    let op13 = "Investitii - subscriere  ";
+    let op14 = "Investitii - rascumparare  ";
+    let op15 = "Asigurare  ";
+    let op16 = "Pensie privata";
   
 
   
@@ -60,84 +67,138 @@ function Pasul1 (){
       </div>
 
       <div className='container col-align-items-center col-5'>
+        
         <div className='row'>
           <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
-            value = "pula"
+            value = {op1}
             onClick={(value) => handleChange(value)}
           >
               {op1}
-              
-
           </button>
         </div>
-
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
-                  name='name'
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op2}
+            onClick={(value) => handleChange(value)}
           >
-              Operatiune fara numerar
+              {op2}
           </button>
         </div>
-
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" > 
-              Credit
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op3}
+            onClick={(value) => handleChange(value)}
+          >
+              {op3}
           </button>
         </div>
-
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Economisire
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op4}
+            onClick={(value) => handleChange(value)}
+          >
+              {op4}
           </button>
         </div>
-
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op5}
+            onClick={(value) => handleChange(value)}
+          >
+              {op5}
           </button>
         </div>
-
-      
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op6}
+            onClick={(value) => handleChange(value)}
+          >
+              {op6}
           </button>
         </div>
-
-      
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op7}
+            onClick={(value) => handleChange(value)}
+          >
+              {op7}
           </button>
         </div>
-
-      
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op8}
+            onClick={(value) => handleChange(value)}
+          >
+              {op8}
           </button>
         </div>
-
-      
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op9}
+            onClick={(value) => handleChange(value)}
+          >
+              {op9}
           </button>
         </div>
-
-      
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op10}
+            onClick={(value) => handleChange(value)}
+          >
+              {op10}
           </button>
         </div>
-
-      
         <div className='row'>
-          <button type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" >
-            Asigurare
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op11}
+            onClick={(value) => handleChange(value)}
+          >
+              {op11}
           </button>
         </div>
+        <div className='row'>
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op12}
+            onClick={(value) => handleChange(value)}
+          >
+              {op12}
+          </button>
+        </div>
+        <div className='row'>
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op13}
+            onClick={(value) => handleChange(value)}
+          >
+              {op13}
+          </button>
+        </div>
+        <div className='row'>
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op14}
+            onClick={(value) => handleChange(value)}
+          >
+              {op14}
+          </button>
+        </div>
+        <div className='row'>
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op15}
+            onClick={(value) => handleChange(value)}
+          >
+              {op15}
+          </button>
+        </div>
+        <div className='row'>
+          <button id="m1" type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false"  autoComplete="off" 
+            value = {op16}
+            onClick={(value) => handleChange(value)}
+          >
+              {op16}
+          </button>
+        </div>
+        
+
+        
 
           <div
             onClick={(event) => handleSumbit(event)}
